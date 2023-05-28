b0338b7e90ed7f17c4208aa2754aba1af0ba2e1a
Ritik Deuja <92029196+deujahritik@users.noreply.github.com>
2023-05-29 01:28:24 +0900

    Update Humidity_temperature.py

--- Humidity_temperature.py
+++ Humidity_temperature.py
@@ -1,24 +1,47 @@
-import Adafruit_DHT
+import os
+import json
 import time
+import board
+import adafruit_dht
 
-# Sensor model (DHT11 or DHT22)
-sensor = Adafruit_DHT.DHT22
+# Initialize the DHT device with data pin connected to GPIO pin 17 (change if necessary)
+dhtDevice = adafruit_dht.DHT22(board.D17, use_pulseio=False)
 
-# GPIO pin connected to the sensor data pin
-sensor_pin = 4
+# Create the folder to store JSON data if it doesn't exist
+data_folder = "JSON_Humidity_Temperature_data"
+os.makedirs(data_folder, exist_ok=True)
 
-try:
-    while True:
-        # Read humidity and temperature from the sensor
-        humidity, temperature = Adafruit_DHT.read_retry(sensor, sensor_pin)
+# File to store the sensor data
+filename = f"{data_folder}/sensor_data.txt"
 
-        if humidity is not None and temperature is not None:
-            print("Temperature: {:.1f}°C".format(temperature))
-            print("Humidity: {:.1f}%".format(humidity))
-        else:
-            print("Failed to retrieve data from the sensor.")
+while True:
+    try:
+        # Read the humidity and temperature values from the sensor
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
 
-        time.sleep(2)  # Wait for 2 seconds before reading again
+        print(
+            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                temperature_f, temperature_c, humidity
+            )
+        )
 
-except KeyboardInterrupt:
-    pass
+        # Create a dictionary with the sensor data
+        sensor_data = {
+            "timestamp": str(time.time()),
+            "temperature_c": temperature_c,
+            "temperature_f": temperature_f,
+            "humidity": humidity
+        }
+            # Append the new data to the file
+        with open(filename, "a") as file:
+            file.write(json.dumps(sensor_data) + "\n")
+
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT sensors can be hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+
+    time.sleep(2.0)
